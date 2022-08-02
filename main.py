60f9f31f84469750bcdc31b4b44bfa104e1d6ae1
jeondoh <jundo1302@gmail.com>
2022-08-02 17:18:06 +0900

    feat: 경로 매개변수
    httPie를 이용한 앤드포인트 테스트

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -11,3 +12,13 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: float, request: Request):
+    print(request.path_params)
+    return {"user_id": user_id}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
