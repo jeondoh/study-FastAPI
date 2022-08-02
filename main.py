2b4ef95851460cdd0b0891506083ceeba234c144
jeondoh <jundo1302@gmail.com>
2022-08-02 17:29:58 +0900

    feat: 쿼리 매개변수

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/users")
+def get_users(limit: int):
+    return {"limit": limit}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/users2")
+def get_users2(limit: Optional[int] = None):
+    return {"limit": limit}
 
 
-@app.get("/users/{user_id}")
-def get_user(user_id: float, request: Request):
-    print(request.path_params)
-    return {"user_id": user_id}
+# is_admin에 yes, 1, True를 넣더라도 is_admin은 True 반환
+@app.get("/users3")
+def get_users3(is_admin: bool, limit: int = 100):
+    return {"is_admin": is_admin, "limit": limit}
 
 
 if __name__ == "__main__":
