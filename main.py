72b14cf75914736bb3247767d8a6455038a62775
jeondoh <jundo1302@gmail.com>
2022-08-02 17:36:19 +0900

    feat: 열거형

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
+from enum import Enum
+
 import uvicorn
 from fastapi import FastAPI
-from typing import Optional
 
 app = FastAPI()
 
 
+class UserLevel(str, Enum):
+    a = "a"
+    b = "b"
+    c = "c"
+
+
 @app.get("/users")
-def get_users(limit: int):
-    return {"limit": limit}
-
-
-@app.get("/users2")
-def get_users2(limit: Optional[int] = None):
-    return {"limit": limit}
-
-
-# is_admin에 yes, 1, True를 넣더라도 is_admin은 True 반환
-@app.get("/users3")
-def get_users3(is_admin: bool, limit: int = 100):
-    return {"is_admin": is_admin, "limit": limit}
+def get_users(grade: UserLevel = UserLevel.a):
+    return {"grade", grade}
 
 
 if __name__ == "__main__":
