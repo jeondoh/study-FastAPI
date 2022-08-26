4943dba3ce5a4e25c5cd39fb45eeedc3d1dd6146
전도현 <jundo1302@gmail.com>
2022-08-26 12:16:40 +0900

    feat: 요청 본문

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
-from enum import Enum
+from typing import Optional, List
 
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl, EmailStr
 
 app = FastAPI()
 
 
-class UserLevel(str, Enum):
-    a = "a"
-    b = "b"
-    c = "c"
+class Item(BaseModel):
+    name: str
+    price: float
+    amount: int = 0
 
 
-@app.get("/users")
-def get_users(grade: UserLevel = UserLevel.a):
-    return {"grade", grade}
+class User(BaseModel):
+    name: str
+    password: str
+    avatar_url: Optional[HttpUrl] = None
+    inventory: List[Item] = []
+
+
+@app.post("/users")
+def create_user(user: User):
+    return user
+
+
+@app.get("/users/me")
+def get_user():
+    fake_user = User(
+        name="FastCampus",
+        password="1234",
+        inventory=[
+            Item(name="전설 무기", price=1_000_000),
+            Item(name="전설 방어구", price=900_000),
+        ],
+    )
+    return fake_user
 
 
 if __name__ == "__main__":
