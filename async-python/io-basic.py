e062b8a3acc05423579b907af574dfcd1cbf4b14
jeondoh <jundo1302@gmail.com>
2023-01-22 23:58:40 +0900

    feat: 기본 프로세스

---
+++ async-python/io-basic.py
@@ -0,0 +1,26 @@
+import time
+import requests
+import os
+import threading
+
+
+# 기본
+def fetcher(session, url):
+    print(f"{os.getpid()} process | {threading.get_ident()} url : {url}")
+    with session.get(url) as response:
+        return response.text
+
+
+def main():
+    urls = ["https://github.com", "https://apple.com"] * 50
+
+    with requests.Session() as session:
+        result = [fetcher(session, url) for url in urls]
+        print(result)
+
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    end = time.time()
+    print(end - start)  # 7.3초
