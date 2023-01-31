9170d6afb02a935ea0d300b8a2f4379b948e9dd9
jeondoh <jundo1302@gmail.com>
2023-01-31 16:19:50 +0900

    feat: collector_book project setup

--- collector_book/app/main.py
+++ collector_book/app/main.py
@@ -1,14 +1,23 @@
-from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI()
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "./index.html", {"request": request, "title": "콜렉터 북북이"}
+    )
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/search", response_class=HTMLResponse)
+async def search(request: Request, q: str):
+    return templates.TemplateResponse(
+        "./index.html", {"request": request, "title": "콜렉터 북북이"}
+    )
